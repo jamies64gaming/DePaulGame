2c1da879b504ea56cdcb1822da63f8238ed0aa44
jamie clarke <jamies64gaming@gmail.com>
2024-04-10 20:44:36 +0100

    added camera zoom

---
+++ DePaul/Assets/CameraManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
+
+public class CameraManager : MonoBehaviour
+{
+    public int[] zoomStages;
+    public float speed;
+    public float zoomLevel;
+
+    public Vector3 startingPos;
+    public Vector3 endingPos;
+
+    public int stage = 0;
+
+    private float currentZoom;
+    private float targetZoom;
+
+    private float sinTime;
+    
+    // Update is called once per frame
+    void UpdateCamera()
+    {
+        float x = Mathf.Lerp(startingPos.x, endingPos.x, zoomLevel/100);
+        float y = Mathf.Lerp(startingPos.y, endingPos.y, zoomLevel/100);
+        float z = Mathf.Lerp(startingPos.z, endingPos.z, zoomLevel/100);
+        Vector3 pos = new Vector3(x,y,z);
+        transform.position = pos;
+    }
+
+    private void Update()
+    {
+        if (zoomLevel != targetZoom)
+        {
+            sinTime += Time.deltaTime * speed;
+            sinTime = Mathf.Clamp(sinTime, 0, Mathf.PI);
+            float t = evaluate(sinTime);
+            zoomLevel = Mathf.Lerp(currentZoom, targetZoom, t);
+            UpdateCamera();
+        }
+        
+        if (Input.GetKeyDown("space"))
+        {
+            SwitchStage(stage++);
+        }
+    }
+    
+    public void SwitchStage(int stage)
+    {
+        if (stage >= zoomStages.Length)
+            return;
+        else
+        {
+            sinTime = 0;
+            currentZoom = targetZoom;
+            targetZoom = zoomStages[stage];
+        }
+
+    }
+
+    public float evaluate(float x)
+    {
+        return 0.5f * Mathf.Sin(x - Mathf.PI / 2f) + .5f;
+    }
+}
